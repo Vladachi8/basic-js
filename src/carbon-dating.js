590eba387dbfd427a84ed2a53d24dc4e2def09d4
Vlad Choporov <Vladachi8@gmail.com>
2020-10-25 22:45:42 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const sample = parseFloat(sampleActivity);
+  if ( sample <= 0 || sample > MODERN_ACTIVITY || !sample || typeof(sampleActivity) !== "string") {
+    return false;
+  } else {
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    let age = Math.log(MODERN_ACTIVITY / sample) / k;
+    age = Math.ceil(age);
+    return age;
+  }
 };
